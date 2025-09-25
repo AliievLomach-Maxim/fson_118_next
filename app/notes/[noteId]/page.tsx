c6f82d4a8e3bd3d02898352e5216c0f1e781e6f7
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2025-09-25 21:47:38 +0300

    +

---
+++ app/notes/[noteId]/page.tsx
@@ -0,0 +1,27 @@
+// SSR
+import { getSingleNote } from '@/lib/api'
+import DetailsPageClient from './DetailsPage.client'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+
+interface Props {
+  params: Promise<{ noteId: string }>
+}
+const Details = async ({ params }: Props) => {
+  const { noteId } = await params
+  // const note = await getSingleNote(noteId)
+
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', noteId],
+    queryFn: () => getSingleNote(noteId),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DetailsPageClient />
+    </HydrationBoundary>
+  )
+}
+
+export default Details
